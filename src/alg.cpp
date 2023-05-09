daa373b2acf4890f0f5f5c706f0851cfbc6af8c9
ZetaGameRus <118940070+ZetaGameRu@users.noreply.github.com>
2023-05-09 13:58:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-    int count = 0;
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value) {
-            count++;
-        }
-    }
-    return count;  
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+      if (arr[i] == value) {
+          count++;
+      }
+  }
+  return count;  
 }
